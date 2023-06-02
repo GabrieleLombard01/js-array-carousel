c83f4573678bb0caceed08a9573bc169e02f5caa
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-02 18:18:17 +0200

    left-btn added

--- js/script.js
+++ js/script.js
@@ -30,3 +30,16 @@ rightBtn.addEventListener('click', function() {
     //Aggiungo class active all'img con l'indice corrispondente
     imges[nowIndex].classList.add('active');
 });
+
+// ASCOLTO sul left-button:
+leftBtn.addEventListener('click', function() {
+
+    //Rimuovo class active all'img con l'indice corrente
+    imges[nowIndex].classList.remove('active');
+
+    //Decremento l'indice corrente
+    nowIndex--;
+
+    //Aggiungo class active all'img con l'indice corrispondente
+    imges[nowIndex].classList.add('active');
+});
