65ff4290e69f93e69aa90a8a237aeab91ee2abed
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-02 18:02:07 +0200

    var index add

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ console.log('JS OK')
 
 //RECUPERO le immagini
 const imges = document.querySelectorAll('#carousel img');
+
+//Preparo la gestione dell'indice delle immagini
+let nowIndex = 0;
+
+//IMG 1 - come attiva
+imges[1].classList.add('active');
