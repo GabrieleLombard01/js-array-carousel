aba646423abdf066815ac9073c7c9af9f17be862
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-02 18:06:40 +0200

    right btn - click function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+//! OPERAZIONI INIZIALI:
+
 //JS RESET
 console.log('JS OK')
+
+//RECUPERO i bottoni
+const rightBtn = document.getElementById('right-btn');
+const leftBtn = document.getElementById('left-btn');
 
 //RECUPERO le immagini
 const imges = document.querySelectorAll('#carousel img');
@@ -9,3 +15,10 @@ let nowIndex = 0;
 
 //IMG 1 - come attiva
 imges[1].classList.add('active');
+
+//! OPERAZIONI LOGICHE:
+
+// ASCOLTO sul right-button:
+rightBtn.addEventListener('click', function() {
+    
+});
